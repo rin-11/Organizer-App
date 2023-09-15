ba90a7b59d7a99bc151d4000bf76185df41cc81e
Rin Kates <erinkates11@gmail.com>
2023-09-14 23:10:10 -0400

    delete button styling

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ const inputBox = document.getElementById('input-box')
 const listContainer = document.getElementById('list-container')
 
 function addTask(){
-    if(inputBox.value === ''){
-        alert("Cannot add an empty list item")
+    if(inputBox.value === ''){ // does not allow empty entry
+        alert("Cannot add an empty list item");
     }
     else{
-        let li = document.createElement('li')
+        let li = document.createElement('li'); // create list item
         li.innerHTML = inputBox.value;
-        listContainer.appendChild(li);
+        listContainer.appendChild(li); // add to list
+        let span = document.createElement('span');
+        span.innerHTML= '\u00d7'
+        li.appendChild(span);
     }
+    inputBox.value = ''; // clear after add
 }
