9446739b1eeff7d2362f174589a7baa3275e169c
Rin Kates <erinkates11@gmail.com>
2023-09-14 23:00:51 -0400

    add button function working

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 const inputBox = document.getElementById('input-box')
 const listContainer = document.getElementById('list-container')
+
+function addTask(){
+    if(inputBox.value === ''){
+        alert("Cannot add an empty list item")
+    }
+    else{
+        let li = document.createElement('li')
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+    }
+}
